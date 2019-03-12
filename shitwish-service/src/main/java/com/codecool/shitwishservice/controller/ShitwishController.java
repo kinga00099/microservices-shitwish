5cffa44ca1b2dd9d8ef3339e1dcb04b40ad7b53b
kinga00099 <kinga00099@gmail.com>
2019-03-12 15:54:01 +0100

    two endpoints added to ShwishController

--- shitwish-service/src/main/java/com/codecool/shitwishservice/controller/ShitwishController.java
+++ shitwish-service/src/main/java/com/codecool/shitwishservice/controller/ShitwishController.java
@@ -1,12 +1,26 @@
 package com.codecool.shitwishservice.controller;
 
+import com.codecool.shitwish.presentservice.model.Present;
 import com.codecool.shitwishservice.service.ServiceCaller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ShitwishController {
     @Autowired
     private ServiceCaller serviceCaller;
 
+    @GetMapping("/presents")
+    public List<Present> getAll(){
+        return serviceCaller.getPresents();
+    }
+
+    @GetMapping("/present/{id}")
+    public Present getAll(@PathVariable Integer id){
+        return serviceCaller.getPresentById(id);
+    }
 }
