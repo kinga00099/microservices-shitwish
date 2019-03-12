63dd1b9f5faf62cb16d21e2e3b1290e74714afed
kinga00099 <kinga00099@gmail.com>
2019-03-12 13:53:38 +0100

    initial commit

---
+++ present-service/src/main/java/com/codecool/shitwish/presentservice/PresentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.shitwish.presentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresentServiceApplication.class, args);
+    }
+
+}
---
+++ shitwish-service/src/main/java/com/codecool/shitwishservice/ShitwishServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.shitwishservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShitwishServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShitwishServiceApplication.class, args);
+    }
+
+}
