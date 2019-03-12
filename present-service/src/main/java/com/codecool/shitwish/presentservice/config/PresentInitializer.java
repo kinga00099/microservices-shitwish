40aea283a801b1d3e83b4188ac861fc94255feb8
manoczar <mano.czar@gmail.com>
2019-03-12 14:32:34 +0100

    Present initializer created

---
+++ present-service/src/main/java/com/codecool/shitwish/presentservice/config/PresentInitializer.java
@@ -0,0 +1,12 @@
+package com.codecool.shitwish.presentservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PresentInitializer {
+
+
+
+
+
+}
