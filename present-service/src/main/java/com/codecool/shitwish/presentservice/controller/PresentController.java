aff8d38d630bec9e26475b093ccb7d9960e37d7d
kinga00099 <kinga00099@gmail.com>
2019-03-12 14:52:04 +0100

    present controller implemented

--- present-service/src/main/java/com/codecool/shitwish/presentservice/controller/PresentController.java
+++ present-service/src/main/java/com/codecool/shitwish/presentservice/controller/PresentController.java
@@ -1,4 +1,26 @@
 package com.codecool.shitwish.presentservice.controller;
 
+import com.codecool.shitwish.presentservice.model.Present;
+import com.codecool.shitwish.presentservice.service.PresentStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PresentController {
+    @Autowired
+    private PresentStorage storage;
+
+    @GetMapping("/getAll")
+    public List<Present> getAll(){
+        return storage.getAll();
+    }
+
+    @GetMapping("/getBy/{id}")
+    public Present getById(@PathVariable("id") Integer id ){
+        return storage.getById(id);
+    }
 }
